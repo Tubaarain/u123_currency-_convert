30f336d409604ffa2f2a7e907e9af4598f62aaff
Tubaarain <tubaarain906@gmail.com>
2024-04-09 01:36:03 +0500

    currency exchange

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let user_amswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: [`USD`, `EUR`, `GBP`, `INR`, `PKR`]
+    },
+    {
+        name: "to",
+        message: "Enter to currency",
+        type: "list",
+        choices: [`USD`, `EUR`, `GBP`, `INR`, `PKR`]
+    },
+    {
+        name: "amount",
+        message: "Enter your amount ",
+        type: "number",
+    },
+]);
+//  console.log(user_amswer)
+let fromAmount = currency[user_amswer.from];
+let toAmount = currency[user_amswer.to];
+let amount = user_amswer.amount;
+console.log(fromAmount);
+console.log(toAmount);
+console.log(amount);
